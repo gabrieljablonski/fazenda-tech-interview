aecc4edf937091a326888cd07197e4d3fe66721f
gabrieljablonski <contact@gabrieljablonski.com>
2024-08-15 21:28:47 -0300

    feat: add discount rate to `calculate_total` function

--- 01-programming-tools/01-git-merge.py
+++ 01-programming-tools/01-git-merge.py
@@ -1,3 +1,3 @@
-def calculate_total(price, tax_rate):
-  total = (1 + tax_rate) * price
+def calculate_total(price, tax_rate, discount_rate=0):
+  total = (1 + tax_rate - discount_rate) * price
   return total
