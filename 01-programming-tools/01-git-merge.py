8e84715e228df40435655acf40be461aa4fa52eb
gabrieljablonski <contact@gabrieljablonski.com>
2024-08-15 21:29:22 -0300

    feat: add decimal places option to `calculate_total` function

--- 01-programming-tools/01-git-merge.py
+++ 01-programming-tools/01-git-merge.py
@@ -1,3 +1,3 @@
-def calculate_total(price, tax_rate):
+def calculate_total(price, tax_rate, decimal_places=2):
   total = (1 + tax_rate) * price
-  return total
+  return round(total, decimal_places)
